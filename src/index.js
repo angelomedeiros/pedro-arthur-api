ef8a6a794de951a6a66f51c1dad65cdcb5253a56
WellysonP <100440445+WellysonP@users.noreply.github.com>
2023-08-16 18:05:31 -0300

    [FEATURE]: adicionado cors

--- src/index.js
+++ src/index.js
@@ -1,12 +1,14 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const cors = require("cors")
 
 app = express()
 
 const guest = require('./Routes/guestRouter')
 
 require('./DataBase/index')
-app.use(bodyParser.json());
+app.use(cors())
+app.use(bodyParser.json())
 app.use('/guest', guest)
 
 
