2ca149640d0965bcfc8be33218e7960b396a78e2
WellysonP <100440445+WellysonP@users.noreply.github.com>
2023-08-19 12:29:23 -0300

    [FIX]: adicionado vercel

---
+++ dist/Query/guestQuery.js
@@ -0,0 +1,3 @@
+module.exports = {
+    SumPeoples: 'SELECT SUM(quantity) AS total FROM guest'
+};
---
+++ dist/index.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const cors = require("cors");
+
+app = express();
+
+const guest = require('./Routes/guestRouter');
+
+require('./DataBase/index');
+app.use(cors());
+app.use(bodyParser.json());
+app.use('/guest', guest);
+
+const port = 3030;
+app.listen(port, function () {
+    console.log("Servidor iniciado na porta ", port);
+});
