1c74dc7508770ae8e9bba14323db08ca35cc00ad
angelo.medeiros@govoll.com <angelo.medeiros@govoll.com>
2023-08-19 16:53:25 -0300

    update vercel json

--- api/index.js
+++ api/index.js
@@ -7,10 +7,11 @@ app = express()
 const guest = require('./Routes/guestRouter')
 
 require('./DataBase/index')
-app.use(cors())
-app.use(bodyParser.json())
-app.use('/guest', guest)
 
+app.use(cors())
+
+app.use(bodyParser.json())
+app.use('/api/guest', guest)
 
 const port = 3030
 app.listen(port, function () {
